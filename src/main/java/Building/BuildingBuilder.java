ed1e7ef7888356b76c1c50818733fdae4ff9a009
DumDumGeniuss <dumdumgenius@gmail.com>
2016-03-19 16:33:21 +0800

    Fixed ReactiveX

--- src/main/java/Building/BuildingBuilder.java
+++ src/main/java/Building/BuildingBuilder.java
@@ -11,7 +11,7 @@ public class BuildingBuilder {
 
 	public Building buildHouse(String name) throws InterruptedException{
 		System.out.println("Start building house , name: "+name);
-		Thread.sleep(5000);
+		Thread.sleep(2000);
 		System.out.println("Finsih building house , name: "+name);
 		return (new House(name));
 	}
@@ -25,12 +25,14 @@ public class BuildingBuilder {
 	
 	public void appendBuilding(List<Building> buildings ,Building building) throws InterruptedException{
 		try{
+			System.out.println("Add "+building.getName());
 			Thread.sleep(3000);
 			buildings.add(building);
-			System.out.println("Add "+building.getName());
+			System.out.println("Complete Adding " + building.getName());
 		}catch(InterruptedException e){
-			buildings.add(building);
 			System.out.println("Add "+building.getName());
+			buildings.add(building);
+			System.out.println("Complete Adding " + building.getName());
 		}finally{
 			
 		}
--- src/main/java/reactive_x/ReactiveXDemo.java
+++ src/main/java/reactive_x/ReactiveXDemo.java
@@ -8,7 +8,11 @@ import java.util.concurrent.TimeUnit;
 
 import Building.Building;
 import Building.BuildingBuilder;
+import Building.House;
 import rx.*;
+import rx.functions.Action;
+import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 public class ReactiveXDemo {
 	public static void main(String args[]) throws InterruptedException {
@@ -19,61 +23,67 @@ public class ReactiveXDemo {
 
 	
 	private static void demo1() {
-		final BuildingReactiveX brx = new BuildingReactiveX( new BuildingBuilder());
-		List<Observable<Building>> houseObservers = new LinkedList<Observable<Building>>();
-		List<Observable<Building>> castleObservers = new LinkedList<Observable<Building>>();
+		
+		final BuildingBuilder bb = new BuildingBuilder();
+		final String[] houseNames = new String[]{"Amy's house","Steven's hosue","Allen's house"};
+		final String[] castleNames = new String[]{"Linda's castle","Allen's castle","Kate's castle"};	
 		final LinkedList<Building> houses = new LinkedList<Building>();
 		final LinkedList<Building> castles = new LinkedList<Building>();
-		String[] houseNames = new String[]{"Amy's house","Steven's house","Allen's house"};
-		String[] castleNames = new String[]{"Linda's castle","Allen's castle","Kate's castle"};
 		
-		//ThreadPoolExecutor exec = new ThreadPoolExecutor(0, 10, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
-		//Scheduler s = Schedulers.from(exec);
-		
-		for(final String name: houseNames){
-			houseObservers.add( brx.buildHouseObserver(name) );
-		}
-		for(final String name: castleNames){
-			castleObservers.add( brx.buildCastleObserver(name) );
-		}
-		
-		Subscriber<Building> sub1 = new Subscriber<Building>() {
-		    public void onCompleted() {
-		        System.out.println("Complete");
-		    }
-		    public void onError(Throwable e) {
+		Observable.create(new Observable.OnSubscribe<Building>() {
 
-		    }
-		    public void onNext(Building house) {
-		    	try {
-					brx.appendBuilding(houses, house);
+			public void call(final Subscriber<? super Building> obs) {
+				for(final String name : houseNames){
+					new Thread(){
+						public void run(){
+							try {
+								obs.onNext( bb.buildHouse(name));
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+					}.start();
+				}
+				for(final String name : castleNames){
+					new Thread(){
+						public void run(){
+							try {
+								obs.onNext( bb.buildCastle(name));
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+					}.start();
+				}
+			}
+		}).subscribe(new Subscriber<Building>(){
+
+			public void onCompleted() {
+				System.out.println("Complete");
+				
+			}
+
+			public void onError(Throwable arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void onNext(Building building) {
+				System.out.println("Complete building: " + building.getName());
+				try {
+					if(building instanceof House){
+						bb.appendBuilding(houses, building);
+					}else{
+						bb.appendBuilding(castles, building);
+					}
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-		    }
-		};
-		Subscriber<Building> sub2 = new Subscriber<Building>() {
-		    public void onCompleted() {
-		        System.out.println("Complete");
-		    }
-		    public void onError(Throwable e) {
-
-		    }
-		    public void onNext(Building castle) {
-		    	try {
-					brx.appendBuilding(castles, castle);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-		    }
-		};
+				
+			}
+			
+		});
 		
-		for(Observable<Building> observer:houseObservers){
-			observer.subscribeOn(Schedulers.from(new ThreadPoolExecutor(0, 10, 30, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>()))).observeOn(Schedulers.computation()).subscribe(sub1);
-		}
-		for(Observable<Building> observer:castleObservers){
-			observer.subscribeOn(Schedulers.from(new ThreadPoolExecutor(0, 10, 30, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>()))).observeOn(Schedulers.computation()).subscribe(sub2);
-		}
 		
-	}	
+	}
 }
